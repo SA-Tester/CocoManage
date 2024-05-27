01a00a3052fc4aea5ebd61d02f71b1207ef709a1
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-05-27 13:28:26 +0530

    tailwind configurations for colors and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  fontFamily: {
+    noto: "'Noto Sans', 'sans-serif'",
+    ABeeZee: "'ABeeZee', 'sans-serif'",
+    Poppins: "'Poppins', 'sans-serif'",
+  },
+  
   theme: {
+    colors: {
+      // transparent: 'transparent',
+      // current: 'currentColor',
+      white: "#ffffff",
+      black: "#000000",
+      badge: "#F5EFE6",
+      green: "#1A4D2E",
+      "light-green": "#4F6F52",
+      grey: "#4B514D",
+      "light-grey": "#D9D9D9",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
