2f920865cf0f70fc7b2f136ddbce74f03ccdcbfe
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-05-28 00:48:13 +0530

    creating a url pattern to upload the photos for verification

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
+    # path('hello-world/', views.hello_world, name='hello_world'),
+    path('upload/', views.FileUploadView.as_view(), name='file-upload'),
 ]
