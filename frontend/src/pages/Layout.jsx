fe0bc9c54caf155c7c012641f8c2506e27fdf5e7
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-05-27 17:01:40 +0530

    rendering common components

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import Navbar2 from '../components/navbar/Navbar2';
+import React from "react";
+import Navbar2 from "../components/navbar/Navbar2";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar2 />
-      <div className="pt-16">
-        {children}
-      </div>
+      <div className="pt-16">{children}</div>
     </div>
   );
-}
+};
 
 export default Layout;
