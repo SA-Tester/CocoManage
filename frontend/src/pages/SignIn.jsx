e8b8d0daf58fbf608bb62a19ecfbffb607950a71
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-05-27 13:43:24 +0530

    Sample SignIn and SignUp pages

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignIn = () => {
+    return (
+        <div>
+            <div className="relative bg-green">
+                <h1>Sign In Page</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <div>
+            <div className="relative bg-green">
+                <h1>Sign Up Page</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
