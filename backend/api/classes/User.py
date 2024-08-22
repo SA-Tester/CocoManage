d1c17cb98f9be00f19352a373074747bce3e5e01
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-08-22 15:48:04 +0530

    User class to handle Login and Logout with updated signup using firebase auth

--- backend/api/classes/User.py
+++ backend/api/classes/User.py
@@ -1,94 +1,163 @@
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.utils import timezone
+from requests.exceptions import HTTPError
+import json
+
 
 class SystemUser:
-    def __init__(self, database_obj, nic, email, password, confirm_password):
-        self.email = email
-        self.nic = nic
-        self.password = password
-        self.confirm_password = confirm_password
-        self.database_obj = database_obj
+    PASSWORD_LENGTH = 8
+    emp_id = None
+    access_level = ""
+    created_at = ""
 
-    # retrieve employee data by NIC
-    def get_employee_by_nic(self):
-        all_employees = self.database_obj.child('Employee').get().val()
+    # Validate user inputs
+    def validate_signup(self, database_obj, nic, email, password, confirm_password):
+        if not email or not nic or not password or not confirm_password:
+            raise ValidationError('All fields are required.')
+        
+        elif self.check_email(database_obj, email):
+            raise ValidationError('The Email already exists. Please use another valid email address.')
+        
+        elif password != confirm_password:
+            raise ValidationError('Passwords do not match.')
+        
+        elif len(password) < self.PASSWORD_LENGTH:
+            raise ValidationError(f"Password should contain at least {self.PASSWORD_LENGTH} characters.") 
+        
+        else:
+            return True
+
+
+    # To be an User, one should be an Employee and should be a Manager or Assistant Manager
+    def checkEligibility(self, database_obj, nic):
+        all_employees = database_obj.child('Employee').get().val()
+        
+        # No employees in the database
         if not all_employees:
-            return None
+            return False
         
         for emp_id, details in all_employees.items():
-            if details.get('nic') == self.nic:
-                return {'employee_id': emp_id, **details}
+            if (details.get('nic') == nic and details.get('position') in ["Manager", "Assistant Manager"]):
+                self.emp_id = emp_id
+                self.access_level = self.get_access_level(details.get('position'))
+                self.created_at = timezone.now().isoformat()
+                return True
             
-        return None
+        return False
+
+
+    # Signup a new user
+    def signup(self, database_obj, auth_obj, email, password):
+        try:
+            auth_obj.create_user_with_email_and_password(email, password)
+            database_obj.child('User').child(self.emp_id).set({
+                "username": email,
+                "password": make_password(password),
+                "access_level": self.access_level,
+                "created_at": self.created_at
+            })
+
+        except Exception as e:
+            raise ValidationError(e)
+
+
+    def login(self, database_obj, auth_obj, email, password):
+        try:
+            user = auth_obj.sign_in_with_email_and_password(email, password)
+            if user != None:
+                signed_in_email = user["email"]
+                refresh_token = user["refreshToken"]
+                local_id = user["localId"]
+
+                data = {
+                    "email": signed_in_email,
+                    "refresh_token": refresh_token,
+                    "local_id": local_id,
+                }
+
+                database_obj.child("LoggedIn").push(data, user['idToken'])
+                self.get_logged_in_user(database_obj, signed_in_email)
+                return {"id_token": user['idToken'], "refresh_token": refresh_token, "access_level": self.access_level}
+            return None
+        
+        except Exception as e:
+            raise ValidationError(e)
     
-    #check whther email already exists
-    def check_email(self):
-        all_users = self.database_obj.child('User').get().val()
+
+    # Logout a user
+    def logout(self, database_obj, auth_obj, id_token):
+        try:
+            User = database_obj.child('LoggedIn').get().val()
+            currentUser = auth_obj.get_account_info(id_token)
+            currentUserEmail = currentUser['users'][0]['email']
+
+            for id, details in User.items():
+                if details['email'] == currentUserEmail:
+                    database_obj.child('LoggedIn').child(id).remove()
+                    auth_obj.current_user = None
+                    return True
+
+        except Exception as e:
+            print(ValidationError(e))
+            return False
+
+
+    # Create accress level
+    def get_access_level(self, position):
+        if(position == "Manager"):
+            return "A1" # Admin Level 1: Managers
+        else:
+            return "A2" # Admin Level 2: Assistant Managers
+        
+
+    # Check whether email already exists
+    def check_email(self, database_obj, email):
+        all_users = database_obj.child('User').get().val()
         if not all_users:
             return False
         
         for _, details in all_users.items():
-            if details.get('username') == self.email:
+            if details.get('username') == email:
                 return True
         
         return False
-
-    # validate user inputs
-    def validate(self):
-        if not self.email or not self.nic or not self.password or not self.confirm_password:
-            raise ValidationError('All fields are required.')
-        
-        if self.check_email():
-            raise ValidationError('The Email already exists. Please use another valid email address.')
-        
-        if self.password != self.confirm_password:
-            raise ValidationError('Passwords do not match.')
-        
-        if len(self.password) < 6:
-            raise ValidationError('Password should contain at least 6 characters.') 
-        
-        self.employee = self.get_employee_by_nic()
-        if not self.employee:
-            raise ValidationError('Employee not found.')
-        
-        if self.employee['position'] not in ["Manager", "Assistant Manager"]:
-            raise ValidationError('Only Managers or Assistant Managers can register.')
-        
-
-    # create a new user   
-    def create_user(self):
-        self.validate()
-        hashed_password = make_password(self.password)
-        user = User.objects.create(username=self.email, password=hashed_password)
-        user.save()
-        return user,self.employee['employee_id'] 
     
-    # save user data to db
-    def save_user_data(self,employee_id,hashed_password, tokens):
-        user_data = {
-            "username": self.email,
-            "password": hashed_password,
-            "position": self.employee['position'],
-            "created_at": timezone.now().isoformat(),
-            "access_token": tokens['access'],
-            "refresh_token": tokens['refresh'],
-        }
-        self.database_obj.child('User').child(employee_id).set(user_data)
 
-    # generate JWT tokens
-    def generate_tokens(self, user):
-        from rest_framework_simplejwt.tokens import RefreshToken
-        refresh = RefreshToken.for_user(user)
-        return{
-            'refresh': str(refresh),
-            'access': str(refresh.access_token)
-        }
+    # Get Logged in Employee ID
+    def get_logged_in_user(self, database_obj, signed_in_email):
+        employee = database_obj.child('User').get().val()
+        for emp_id, details in employee.items():
+            if details.get('username') == signed_in_email:
+                self.emp_id = emp_id
+                self.access_level = details.get('access_level')
+                return True
+        return False
     
-    # run the process
-    def execute(self):
-        user, employee_id = self.create_user()
-        tokens = self.generate_tokens(user)
-        hashed_password = user.password
-        self.save_user_data(employee_id, hashed_password, tokens)
+
+    # Check whether the user is logged in and token is valid
+    def isValidToken(self, auth_obj, id_token, refresh_token):
+        try:
+            auth_obj.get_account_info(id_token)
+            return id_token
+        
+        except HTTPError as http_err:
+            json_obj = json.loads(http_err.args[1])
+            if json_obj["error"]["message"] == "INVALID_ID_TOKEN":
+                id_token = self.refresh_token(auth_obj, refresh_token)
+                return id_token
+            
+            print(json_obj)
+        
+        except Exception as e:
+            print(e)
+            return None
+        
+
+    # Refresh the token
+    def refresh_token(self, auth_obj, refresh_token):
+        try:
+            user = auth_obj.refresh(refresh_token)
+            return user['idToken']
+        except Exception as e:
+            raise ValidationError(e)
